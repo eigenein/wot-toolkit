0a91308eb93fe403876d69d00cc138b62a974eef
Pavel Perestoronin <eigenein@gmail.com>
2014-06-23 20:08:12 +0300

    Initial robot code.

--- v2/robot.py
+++ v2/robot.py
@@ -4,22 +4,101 @@
 import sys; sys.dont_write_bytecode = True
 
 import argparse
+import itertools
 import json
-import pathlib
 import logging
+import pathlib
+import random
+import time
 
 import requests
 
 import shared
 
 
+LIMIT = 100
+SLEEP_TIME = [None, 1.0, 60.0, 600.0, 3600.0]
+
+
 runtime_info = {
-    "version": 0
+    "account_id": 1,  # start account ID
+    "iteration": 1,  # loop iteration number
+    "version": 0,  # runtime info version
 }
 
 
 def main(args):
-    pass
+    try:
+        run_loop()
+    except KeyboardInterrupt:
+        logging.warning("Loop is interrupted by user.")
+
+
+def run_loop():
+    logging.info("Running loop…")
+
+    while True:
+        logging.info("Starting loop iteration…")
+
+        collect_stats(runtime_info["account_id"])
+
+        logging.info("Loop iteration is finished.")
+        runtime_info["iteration"] += 1
+        runtime_info["last_account_id"] = runtime_info["account_id"]
+        runtime_info["account_id"] = 1
+
+
+def collect_stats(start):
+    session = requests.Session()
+    for base_account_id in itertools.count(start, LIMIT):
+        runtime_info["account_id"] = base_account_id
+        account_ids = range(base_account_id, base_account_id + LIMIT)
+        logging.debug("Accounts: %s.", account_ids)
+        data = make_request(session, account_ids)
+        if data is None:
+            continue
+        if process_data(data):
+            continue
+        else:
+            break
+
+
+def make_request(session, account_ids):
+    account_ids = ",".join(map(str, account_ids))
+    for attempt_number in range(5):
+        if SLEEP_TIME[attempt_number] is not None:
+            logging.warning("Attempt #%d. Sleeping…")
+            time.sleep(SLEEP_TIME[attempt_number])
+        response = session.get("http://api.worldoftanks.ru/wot/account/tanks/", params={
+            "application_id": shared.APPLICATION_ID,
+            "account_id": account_ids,
+            "fields": "statistics.wins,statistics.battles,tank_id",
+        })
+        if response.status_code != requests.codes.ok:
+            logging.warning("Status: %d.", response.status_code)
+            continue
+        payload = response.json()
+        if "data" not in payload:
+            logging.warning("No data in payload.")
+            continue
+        return payload["data"]
+    logging.error("All attempts failed for accounts %s.", account_ids)
+    return None
+
+
+def process_data(data):
+    all_null = True
+    data = sorted(data.items())
+    for account_id, vehicles in data:
+        account_id = int(account_id)
+        logging.debug("Account #%d.", account_id)
+        runtime_info["account_id"] = account_id
+        if vehicles is None:
+            logging.warning("Account #%d is null.", account_id)
+            continue
+        all_null = False
+        pass
+    return not all_null
 
 
 def init_runtime_info(args):
--- v2/shared.py
+++ v2/shared.py
@@ -4,6 +4,6 @@
 import base64
 import logging
 
-exec(base64.b64decode(b"YXBwbGljYXRpb25faWQgPSAiOGVkMDBhZTYzMTlkNmQyYmI3ODYxNmNiNGJiNDg5OWQi"))
+exec(base64.b64decode(b"QVBQTElDQVRJT05fSUQgPSAiOGVkMDBhZTYzMTlkNmQyYmI3ODYxNmNiNGJiNDg5OWQi"))
 
 logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.DEBUG)
