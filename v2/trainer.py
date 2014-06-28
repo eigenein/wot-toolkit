6f7198ff06751f2cd7702b444ce3d8a80da7b407
Pavel Perestoronin <eigenein@gmail.com>
2014-06-28 19:57:11 +0300

    Use CSC matrices

--- v2/trainer.py
+++ v2/trainer.py
@@ -8,6 +8,7 @@ import logging
 
 import msgpack
 import requests
+import numpy
 import scipy.sparse
 
 import shared
@@ -18,7 +19,7 @@ def main(args):
     logging.info("Tank rows: %d entries.", len(tank_rows))
 
     logging.info("Make rating matrix.")
-    y, r = get_rating_matrix(args.input, tank_rows)
+    y, r = get_rating_matrix(args.input, tank_rows, args.account_number, args.tank_number)
 
 
 def get_tank_rows():
@@ -34,29 +35,45 @@ def get_tank_rows():
     return {tank_id: i for i, tank_id in enumerate(tank_ids)}
 
 
-def get_rating_matrix(input, tank_rows):
-    account_number = 0
-    y_data, r_data, indices, indptr = [], [], [], []
+def get_rating_matrix(input, tank_rows, account_number, tank_number):
+    # Initialize arrays.
+    y_data = numpy.zeros(tank_number)
+    r_data = numpy.zeros(tank_number, numpy.bool)
+    indices = numpy.zeros(tank_number, numpy.int)
+    indptr = numpy.zeros(account_number, numpy.int)
+    # Fill up arrays.
+    tank_counter = 0
     for i, obj in enumerate(msgpack.Unpacker(args.input)):
-        account_number += 1
         # Unpack object.
         account_id, *stats = obj
         # Append column.
-        indptr.append(len(indices))
+        indptr[i] = tank_counter
         # Append column values.
         for j in range(0, len(stats), 3):
             tank_id, wins, delta = stats[j:j+3]
             battles = 2 * wins - delta
             rating = wins / battles
             # Append values.
-            y_data.append(rating)
-            r_data.append(1.0)
-            indices.append(tank_rows[tank_id])
+            y_data[tank_counter] = rating
+            r_data[tank_counter] = 1
+            indices[tank_counter] = tank_rows[tank_id]
+            tank_counter += 1
         # Log progress.
         position = args.input.tell()
         if i % 100000 == 0:
             logging.info("%d objects | %.1f MiB", i, position / 1048576.0)
-    return None, None
+    # Truncate arrays.
+    logging.info("Tank counter: %d.", tank_counter)
+    # Convert to matrices.
+    y = scipy.sparse.csc_matrix((y_data, indices, indptr))
+    del y_data
+    r = scipy.sparse.csc_matrix((r_data, indices, indptr))
+    del r_data
+    del indices
+    del indptr
+    logging.info("Y shape: %r.", y.shape)
+    logging.info("R shape: %r.", r.shape)
+    return y, r
 
 
 if __name__ == "__main__":
@@ -66,6 +83,22 @@ if __name__ == "__main__":
         help="input file",
         metavar="<input.msgpack.gz>",
         type=shared.GZipFileType("rb"),
+    )
+    parser.add_argument(
+        "-a",
+        dest="account_number",
+        help="exact account number",
+        metavar="<number>",
+        required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "-t",
+        dest="tank_number",
+        help="exact total tank number (across all accounts)",
+        metavar="<number>",
+        required=True,
+        type=int,
     )
     args = parser.parse_args()
     logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
