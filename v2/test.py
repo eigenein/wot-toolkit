50b2c5d4742e8c66c2ce9951f5503eddb5499c23
Pavel Perestoronin <eigenein@gmail.com>
2014-06-28 05:10:21 +0300

    Add test script

---
+++ v2/test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import sys; sys.dont_write_bytecode = True
+
+import argparse
+import collections
+import logging
+
+import msgpack
+
+import shared
+
+
+def main(args):
+    counter = collections.Counter()
+    for i, obj in enumerate(msgpack.Unpacker(args.input)):
+        # Check object.
+        tank_number, mod = divmod(len(obj), 3)
+        assert mod == 1, "invalid object length"
+        # Count statistics.
+        counter["object_number"] += 1
+        counter["tank_number"] += tank_number
+        # Log progress.
+        position = args.input.tell()
+        if i % 100000 == 0:
+            logging.info("%d objects | %.1f MiB", i, position / 1048576.0)
+    logging.info("Object number: %d.", counter["object_number"])
+    logging.info("Tank number: %d.", counter["tank_number"])
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Test statistics file.")
+    parser.add_argument(
+        dest="input",
+        help="input file",
+        metavar="<input.msgpack.gz>",
+        type=shared.GZipFileType("rb"),
+    )
+    args = parser.parse_args()
+    logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
+    try:
+        main(args)
+    except KeyboardInterrupt:
+        logging.warning("Interrupted by user.")
