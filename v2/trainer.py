583195171742b0e1aa56f23ccf53681e193c2f06
Pavel Perestoronin <eigenein@gmail.com>
2014-06-28 22:02:00 +0300

    Initial gradient descent

--- v2/trainer.py
+++ v2/trainer.py
@@ -20,6 +20,8 @@ def main(args):
 
     logging.info("Make rating matrix.")
     y, r = get_rating_matrix(args.input, tank_rows, args.account_number, args.total_tank_number)
+    x, theta = get_parameters(len(tank_rows), args.account_number, args.num_features)
+    gradient_descent(y, r, x, theta, args.l, args.num_iterations)
 
 
 def get_tank_rows():
@@ -44,25 +46,28 @@ def get_rating_matrix(input, tank_rows, account_number, total_tank_number):
     indptr = numpy.zeros(account_number + 1, numpy.int)
     # Fill up arrays.
     tank_counter = 0
-    for i, obj in enumerate(msgpack.Unpacker(args.input)):
-        # Unpack object.
-        account_id, *stats = obj
-        # Append column.
-        indptr[i] = tank_counter
-        # Append column values.
-        for j in range(0, len(stats), 3):
-            tank_id, wins, delta = stats[j:j+3]
-            battles = 2 * wins - delta
-            rating = wins / battles
-            # Append values.
-            y_data[tank_counter] = rating
-            r_data[tank_counter] = 1
-            indices[tank_counter] = tank_rows[tank_id]
-            tank_counter += 1
-        # Log progress.
-        position = args.input.tell()
-        if i % 100000 == 0:
-            logging.info("%d objects | %.1f MiB", i, position / 1048576.0)
+    try:
+        for i, obj in enumerate(msgpack.Unpacker(args.input)):
+            # Unpack object.
+            account_id, *stats = obj
+            # Append column.
+            indptr[i] = tank_counter
+            # Append column values.
+            for j in range(0, len(stats), 3):
+                tank_id, wins, delta = stats[j:j+3]
+                battles = 2 * wins - delta
+                rating = wins / battles
+                # Append values.
+                y_data[tank_counter] = rating
+                r_data[tank_counter] = 1
+                indices[tank_counter] = tank_rows[tank_id]
+                tank_counter += 1
+            # Log progress.
+            position = args.input.tell()
+            if i % 100000 == 0:
+                logging.info("%d objects | %.1f MiB", i, position / 1048576.0)
+    except KeyboardInterrupt:
+        logging.warning("Interrupted by user.")
     indptr[account_number] = tank_counter
     # Truncate arrays.
     logging.info("Tank counter: %d.", tank_counter)
@@ -78,6 +83,48 @@ def get_rating_matrix(input, tank_rows, account_number, total_tank_number):
     r.indptr = indptr
     logging.info("R: %r.", r)
     return y, r
+
+
+def get_parameters(tank_number, account_number, feature_number):
+    x = 0.001 * numpy.random.rand(tank_number, feature_number)
+    logging.info("X shape: %r.", x.shape)
+    theta = 0.001 * numpy.random.rand(account_number, feature_number)
+    logging.info("Theta shape: %r.", theta.shape)
+    return x, theta
+
+
+def gradient_descent(y, r, x, theta, l, num_iterations):
+    logging.info("Gradient descent.")
+    alpha, previous_cost = 0.001, float("+inf")
+    try:
+        for i in range(num_iterations):
+            x_new, theta_new = do_step(y, r, x, theta, l, alpha)
+            current_cost = cost(y, r, x_new, theta_new, l)
+            logging.info(
+                "#%d | cost: %.3f | delta: %.6f | alpha: %f",
+                i, current_cost, current_cost - previous_cost, alpha)
+            if current_cost < previous_cost:
+                alpha *= 1.05
+                x, theta = x_new, theta_new
+            else:
+                logging.warning("Step: #%d.", i)
+                logging.warning("Reset alpha: %f.", alpha)
+                logging.warning("Cost: %.3f.", current_cost)
+                alpha *= 0.5
+            previous_cost = current_cost
+    except KeyboardInterrupt:
+        logging.warning("Gradient descent is interrupted by user.")
+
+
+def cost(y, r, x, theta, l):
+    return (((x.dot(theta.T) - y) * r) ** 2).sum() / 2.0 + l * (theta ** 2).sum() / 2.0 + l * (x ** 2).sum() / 2.0
+
+
+def do_step(y, r, x, theta, l, alpha):
+    diff = (x.dot(theta.T) - y) * r
+    x_grad = diff.dot(theta) + l * x
+    theta_grad = diff.T.dot(x) + l * theta
+    return (x - alpha * x_grad, theta - alpha * theta_grad)
 
 
 if __name__ == "__main__":
@@ -102,6 +149,31 @@ if __name__ == "__main__":
         help="exact total tank number (across all accounts)",
         metavar="<number>",
         required=True,
+        type=int,
+    )
+    parser.add_argument(
+        "--num-features",
+        default=4,
+        dest="num_features",
+        help="features number (default: %(default)s)",
+        metavar="<number>",
+        type=int,
+    )
+    parser.add_argument(
+        "-l",
+        "--lambda",
+        default=1.0,
+        dest="l",
+        help="regularization parameter (default: %(default)s)",
+        metavar="<lambda>",
+        type=float,
+    )
+    parser.add_argument(
+        "--num-iterations",
+        default=1000,
+        dest="num_iterations",
+        help="number of iterations",
+        metavar="<number>",
         type=int,
     )
     args = parser.parse_args()
