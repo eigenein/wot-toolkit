ecb53d49770f594fa383d24ec6575310befc24db
Pavel Perestoronin <eigenein@gmail.com>
2014-07-03 01:08:05 +0300

    Accept tanks.json

--- v2/trainer.py
+++ v2/trainer.py
@@ -4,6 +4,7 @@
 import sys; sys.dont_write_bytecode = True
 
 import argparse
+import json
 import logging
 
 import msgpack
@@ -18,30 +19,23 @@ DTYPE = numpy.float32
 
 
 def main(args):
-    tank_rows = get_tank_rows()
-    logging.info("Tank rows: %d entries.", len(tank_rows))
+    tanks = get_tanks(args.tanks)
+    logging.info("Tanks: %d.", len(tanks))
 
     logging.info("Make rating matrix.")
-    y = get_rating_matrix(args.input, tank_rows, args.account_number, args.total_tank_number)
-    x, theta = get_parameters(len(tank_rows), args.account_number, args.num_features)
-    gradient_descent(y, x, theta, args.l, args.num_iterations)
+    y = get_rating_matrix(args.input, tanks, args.account_number, args.total_tank_number)
+    x, theta = get_parameters(len(tanks), args.account_number, args.feature_number)
+    # gradient_descent(y, x, theta, args.l, args.iteration_number)
 
 
-def get_tank_rows():
-    response = requests.get(
-        "http://api.worldoftanks.ru/wot/encyclopedia/tanks/",
-        params={
-            "application_id": shared.APPLICATION_ID,
-            "fields": "tank_id",
-        },
-    )
-    response.raise_for_status()
-    tank_ids = sorted(map(int, response.json()["data"]))
-    return {tank_id: i for i, tank_id in enumerate(tank_ids)}
+def get_tanks(tanks):
+    tanks = json.load(tanks)
+    tanks = {int(tank_id): tank for tank_id, tank in tanks.items()}
+    return tanks
 
 
-def get_rating_matrix(input, tank_rows, account_number, total_tank_number):
-    tank_number = len(tank_rows)
+def get_rating_matrix(input, tanks, account_number, total_tank_number):
+    tank_number = len(tanks)
     # Initialize arrays.
     data = numpy.zeros(total_tank_number, dtype=DTYPE)
     indices = numpy.zeros(total_tank_number, numpy.int)
@@ -61,11 +55,11 @@ def get_rating_matrix(input, tank_rows, account_number, total_tank_number):
                 rating = wins / battles
                 # Append values.
                 data[tank_counter] = rating
-                indices[tank_counter] = tank_rows[tank_id]
+                indices[tank_counter] = tanks[tank_id]["row"]
                 tank_counter += 1
             # Log progress.
             position = args.input.tell()
-            if i % 100000 == 0:
+            if i % 10000 == 0:
                 logging.info("%d objects | %.1f MiB", i, position / 1048576.0)
     except KeyboardInterrupt:
         logging.warning("Interrupted by user.")
@@ -80,19 +74,17 @@ def get_rating_matrix(input, tank_rows, account_number, total_tank_number):
 
 def get_parameters(tank_number, account_number, feature_number):
     x = numpy.random.rand(tank_number, feature_number).astype(DTYPE)
-    x *= 0.001
     logging.info("X shape: %r.", x.shape)
     theta = numpy.random.rand(account_number, feature_number).astype(DTYPE)
-    theta *= 0.001
     logging.info("Theta shape: %r.", theta.shape)
     return x, theta
 
 
-def gradient_descent(y, x, theta, l, num_iterations):
+def gradient_descent(y, x, theta, l, iteration_number):
     logging.info("Gradient descent.")
     alpha, previous_cost = 0.001, float("+inf")
     try:
-        for i in range(num_iterations):
+        for i in range(iteration_number):
             x_new, theta_new = step(x, theta, y, l, alpha)
             current_cost = cost(x_new, theta_new, y, l)
             logging.info(
@@ -109,54 +101,6 @@ def gradient_descent(y, x, theta, l, num_iterations):
             previous_cost = current_cost
     except KeyboardInterrupt:
         logging.warning("Gradient descent is interrupted by user.")
-
-
-def cost(x, theta, y, l):
-    diff, diff_t, x_theta = make_diff(x, theta, y)
-    diff.data **= 2
-    current_cost = diff.data.sum() / 2.0 + l * (theta ** 2).sum() / 2.0 + l * (x ** 2).sum() / 2.0
-    diff.data **= 0.5
-    revert(diff, x_theta)
-    return current_cost
-
-
-def step(x, theta, y, l, alpha):
-    diff, diff_t, x_theta = make_diff(x, theta, y)
-
-    x_grad = diff.dot(theta) + l * x
-    theta_grad = diff_t.dot(x) + l * theta
-
-    x_new = x - alpha * x_grad
-    theta_new = theta - alpha * theta_grad
-
-    revert(diff, x_theta)
-    return (x_new, theta_new)
-
-
-def nonzero(y):
-    for col in range(y.shape[1]):
-        for i in range(y.indptr[col], y.indptr[col + 1]):
-            yield i, y.indices[i], col
-
-
-def make_diff(x, theta, y):
-    # Make transposed sparse matrix.
-    y_t = scipy.sparse.csr_matrix(y.shape[::-1], dtype=DTYPE)
-    y_t.data, y_t.indices, y_t.indptr = y.data, y.indices, y.indptr
-    # Make diff.
-    y.data *= -1  # diff = -y
-    x_theta = numpy.ndarray(y.data.size, dtype=DTYPE)
-    for i, row, col in nonzero(y):  # TODO: optimize
-        x_theta[i] = (x[row] * theta[col]).sum()  # TODO: optimize
-    y.data += x_theta  # diff = x.dot(theta.T) * r - y
-    # Return both diff and transposed diff.
-    return y, y_t, x_theta
-
-
-def revert(diff, x_theta):
-    # Revert make_diff.
-    diff.data -= x_theta
-    diff.data *= -1
 
 
 if __name__ == "__main__":
@@ -186,7 +130,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--num-features",
         default=4,
-        dest="num_features",
+        dest="feature_number",
         help="features number (default: %(default)s)",
         metavar="<number>",
         type=int,
@@ -203,10 +147,18 @@ if __name__ == "__main__":
     parser.add_argument(
         "--num-iterations",
         default=1000,
-        dest="num_iterations",
+        dest="iteration_number",
         help="number of iterations",
         metavar="<number>",
         type=int,
+    )
+    parser.add_argument(
+        "--tanks",
+        dest="tanks",
+        help="tank list (from API)",
+        metavar="<tanks.json>",
+        required=True,
+        type=argparse.FileType("rt"),
     )
     args = parser.parse_args()
     logging.basicConfig(format="%(asctime)s [%(levelname)s] %(message)s", level=logging.INFO)
