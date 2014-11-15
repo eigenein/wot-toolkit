9443f865f8f41668775a3e6106563836467d04ba
Pavel Perestoronin <eigenein@gmail.com>
2014-11-15 16:59:52 +0300

    Add get script v4

---
+++ v4/get.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import itertools
+import logging
+import sys
+
+from operator import itemgetter
+from time import sleep, time
+from random import normalvariate
+
+import click
+import requests
+
+
+@click.command()
+@click.option("--app-id", default="demo", help="application ID", metavar="<application ID>", show_default=True)
+@click.option("--start-id", default=1, help="start account ID", metavar="<account ID>", show_default=True, type=int)
+@click.option("--end-id", default=40000000, help="end account ID", metavar="<account ID>", show_default=True, type=int)
+@click.option("--log-file", default=sys.stderr, help="log file", metavar="<file>", type=click.File("wt"))
+@click.argument("output", type=click.File("wb"))
+def main(app_id, start_id, end_id, log_file, output):
+    """Gets account statistics dump."""
+    logging.basicConfig(format="%(asctime)s (%(module)s) %(levelname)s %(message)s", level=logging.INFO, stream=log_file)
+
+    api, start_time = Api(app_id), time()
+    for account_ids in chop(range(start_id, end_id), 100):
+        account_tanks = sorted(api.account_tanks(account_ids), key=itemgetter(0))  # sort by account ID
+        # Print statistics.
+        aps = account_ids[-1] / (time() - start_time)
+        logging.info("#%d aps: %.1f apd: %.0f", account_ids[-1], aps, aps * 86400.0)
+
+
+class Api:
+    """Wargaming Public API interface."""
+
+    def __init__(self, app_id):
+        self.app_id = app_id
+        self.session = requests.Session()
+
+    def account_tanks(self, account_ids):
+        """Gets account tanks."""
+        data = self.make_request(
+            "account/tanks",
+            account_id=self.make_account_id(account_ids),
+            fields="statistics,tank_id",
+        )
+        for account_id, tanks in data.items():
+            if tanks:
+                # Sort by tank ID.
+                yield int(account_id), sorted(tanks, key=itemgetter("tank_id"))
+
+    def make_request(self, method, **kwargs):
+        """Makes API request."""
+        params = dict(kwargs, application_id=self.app_id)
+        backoff = exponential_backoff(0.1, 600.0, 2.0, 0.1)
+        for sleep_time in backoff:
+            response = self.session.get("http://api.worldoftanks.ru/wot/%s/" % method, params=params)
+            if response.status_code == requests.codes.ok:
+                json = response.json()
+                if json["status"] == "ok":
+                    return json["data"]
+                logging.warning("API error: %s", json["error"]["message"])
+            else:
+                logging.warning("HTTP status: %d", response.status_code)
+            logging.warning("sleep %.1fs", sleep_time)
+            sleep(sleep_time)
+
+
+    @staticmethod
+    def make_account_id(account_ids):
+        """Makes account_id value."""
+        return ",".join(map(str, account_ids))
+
+
+def chop(iterable, length):
+    """Splits iterable into chunks."""
+    iterable = iter(iterable)
+    while True:
+        chunk = list(itertools.islice(iterable, length))
+        if not chunk:
+            break
+        yield chunk
+
+
+def exponential_backoff(minimum, maximum, factor, jitter):
+    """Exponential Backoff Algorithm."""
+    value = minimum
+    while True:
+        yield value
+        value = value * factor + jitter * normalvariate(0.0, 1.0)
+        if value > maximum:
+            value = maximum
+        elif value < minimum:
+            value = minimum
+
+
+if __name__ == "__main__":
+    main()
